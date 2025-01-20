d3d93b5490b26ecb1cdf03a5456018f2a1e778cc
Marushini <marushinibabu2203@gmail.com>
2025-01-20 19:19:33 +0530

    done

--- models/User.js
+++ models/User.js
@@ -1,13 +1,15 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs"); // Use bcryptjs instead of bcrypt
 
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
 
+// Hash the password before saving the user document
 UserSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
+  // Use bcryptjs to hash the password
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
